091561568a99c2990e18f34da4bb843c9f63f2f4
Sharad-Banga <sharadiit03@gmail.com>
2025-12-15 22:19:44 +0530

    room manager update

--- src/core/RoomManager.ts
+++ src/core/RoomManager.ts
@@ -1,7 +1,35 @@
 class RoomManager{
 
+  private static instance: RoomManager;
+  private rooms: Map<string, any>;
   constructor(){
-
+    this.rooms = new Map();
   }
+
+  public static getInstance(): RoomManager {
+    if (!RoomManager.instance) {
+      RoomManager.instance = new RoomManager();
+    }   
+    return RoomManager.instance;
+  }
+
+  public join(roomId: string, socket: any): void {
+    if (!this.rooms.has(roomId)) {
+      this.rooms.set(roomId, new Set());
+    }
+    this.rooms.get(roomId).add(socket); 
+  }
+
+  public leave(roomId: string, socket: any): void {
+    if (this.rooms.has(roomId)) {
+      this.rooms.get(roomId).delete(socket);
+      if (this.rooms.get(roomId).size === 0) {
+        this.rooms.delete(roomId);
+      }
+    }
+  }
+
   
 }
+
+export const roomManager = RoomManager.getInstance();
