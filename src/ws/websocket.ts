acaac91c028e09389e0a4baad1ffb18a6a6c779e
Sharad-Banga <sharadiit03@gmail.com>
2025-12-16 22:45:11 +0530

    adding ws code , will debug tomorrow

--- src/ws/websocket.ts
+++ src/ws/websocket.ts
@@ -1,4 +1,6 @@
 import WebSocket,{WebSocketServer} from "ws";
+import { roomManager } from "../core/RoomManager.js";
+import { pubsubManager } from "../core/PubsubManager.js";
 
 export function createWebSocket(server:any){
 
@@ -6,16 +8,31 @@ export function createWebSocket(server:any){
 
   ws.on('connection',function(socket){
 
-    socket.on('message',function(message){
+    socket.on('message',async function(message){
       const parsedData = JSON.parse(message.toString());
 
       if(parsedData.type == "join-room"){
 
+        roomManager.join(parsedData.roomId , socket);
+        pubsubManager.subscribe(parsedData.roomId);
+
+        pubsubManager.on(`room:${parsedData.roomId}`, (payload) => {
+          for (const ws of roomManager.getMembers(parsedData.roomId)) {
+            if (ws.readyState === WebSocket.OPEN) {
+              ws.send(JSON.stringify(payload));
+            }
+          }
+        });
       }
 
 
       if(parsedData.type == "chat"){
-        
+        await pubsubManager.publish(parsedData.roomId, {
+          type: "chat",
+          roomId: parsedData.roomId,
+          message: parsedData.message,
+          ts: Date.now()
+        });
       }
     })
     
