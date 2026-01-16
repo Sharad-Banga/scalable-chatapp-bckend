3e572bd07114a55c5edd932321a8e580d8a758b3
Sharad-Banga <sharadiit03@gmail.com>
2026-01-17 00:00:25 +0530

    deploying

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,10 @@
-import fs from 'fs';
-import https from 'https';
-import { WebSocketServer } from 'ws';
+import http from 'http';
+import { createWebSocket } from './ws/websocket.js';
 
-const server = https.createServer({
-  key: fs.readFileSync('./ssl/private.key'),  // relative Linux path
-  cert: fs.readFileSync('./ssl/cert.pem')
-});
+const server = http.createServer();
 
-const wss = new WebSocketServer({ server });
+createWebSocket(server);
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('message', (msg) => ws.send(`Echo: ${msg}`));
-  ws.send('Welcome!');
-});
-
-server.listen(3001, () => console.log('WSS 3001'));
+server.listen(3001,()=>{
+  console.log("server is listening at port 3001");
+})
