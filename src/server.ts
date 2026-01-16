2212664c1e1a95c4eabb75e001c7a8fae2040e03
Sharad-Banga <sharadiit03@gmail.com>
2026-01-16 23:50:44 +0530

    deploying

--- src/server.ts
+++ src/server.ts
@@ -2,43 +2,17 @@ import fs from 'fs';
 import https from 'https';
 import { WebSocketServer } from 'ws';
 
-// Load SSL certificate and key
 const server = https.createServer({
-  key: fs.readFileSync('C:\\Users\\hp5cd\\ssl\\private.key'),
-  cert: fs.readFileSync('C:\\Users\\hp5cd\\ssl\\cert.pem')
+  key: fs.readFileSync('./ssl/private.key'),  // relative Linux path
+  cert: fs.readFileSync('./ssl/cert.pem')
 });
 
-// Create WebSocket server on top of HTTPS
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  
-  ws.on('message', (message) => {
-    console.log('Received:', message.toString());
-    ws.send(`Echo: ${message.toString()}`);
-  });
-
-  ws.send('Welcome to the secure WebSocket server!');
+  ws.on('message', (msg) => ws.send(`Echo: ${msg}`));
+  ws.send('Welcome!');
 });
 
-// Start server
-server.listen(3001, () => {
-  console.log('WSS running at https://localhost:3001');
-});
-
-
-// import https from 'https';
-// import fs from 'fs';
-// import { createWebSocket } from './ws/websocket.js';
-
-// const server = https.createServer({
-//   key: fs.readFileSync('/path/to/private.key'),
-//   cert: fs.readFileSync('/path/to/cert.crt')
-// });
-
-// createWebSocket(server);
-
-// server.listen(3001, () => {
-//   console.log('Secure WebSocket server listening on 3001');
-// });
+server.listen(3001, () => console.log('WSS 3001'));
